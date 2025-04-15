133174a8354703695523d5ad16c360bb4fc91932
Muhammad <zoirovm823@gmail.com>
2025-04-15 15:00:52 +0500

    train.ts

--- src/train.ts
+++ src/train.ts
@@ -1,2 +1,26 @@
-console.log("Hello World")
+                                // TASK L: 
+// Shunday function yozing, u string qabul qilsin va string ichidagi hamma sozlarni chappasiga yozib va sozlar ketma-ketligini buzmasdan stringni qaytarsin.
+// MASALAN: reverseSentence("we like coding!") return "ew ekil gnidoc";
+
+function teskari(harf: string){
+    return harf.split("").reverse().join("")
+}
+console.log(teskari("Muhammadjon"))
+
+
+
+                                // TASK K:
+
+// Shunday function yozing, u string qabul qilsin va string ichidagi unli harflar sonini qaytarsin.
+// MASALAN: countVowels("string") return 1;
+
+
+// function unliHarfSoni(son : string): number{
+//     const unliHarflar: string = "aeiou";
+//     return unliHarflar
+//     .split("")
+//     .filter((harf: string) => son.toLocaleLowerCase().includes(harf)).length;
+// }
+// console.log(unliHarfSoni("Muhammadjon"));
+
 
