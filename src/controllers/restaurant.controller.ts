375390f2c0a9eac93738e0029b336915350c57c6
Muhammad <zoirovm823@gmail.com>
2025-05-07 22:32:47 +0500

    fix: modify restaurant login and signup process

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -1,73 +1,71 @@
-import { Request, Response } from "express"
+import { Request, Response } from "express";
 import { T } from "../libs/types/common";
-import MemberService from "../models/Member.service"
+import MemberService from "../models/Member.service";
 import { LoginInput, MemberInput } from "../libs/types/member";
-import { MemberType } from "../libs/enums/member.enum"
+import { MemberType } from "../libs/enums/member.enum";
 
 const restaurantController: T = {};
 restaurantController.goHome = (req: Request, res: Response) => {
-    try {
-        console.log("goHome")
-        res.send("Home Page");
-    } catch (err) {
-        console.log('Error, goHome:', err);
-    }
+  try {
+    console.log("goHome");
+    res.send("Home Page");
+  } catch (err) {
+    console.log("Error, goHome:", err);
+  }
 };
 
 restaurantController.getLogin = (req: Request, res: Response) => {
-    try {
-        console.log("getLogin")
-        res.send("Login Page");
-    } catch (err) {
-        console.log('Error, getLogin:', err);
-    }
+  try {
+    console.log("getLogin");
+    res.send("Login Page");
+  } catch (err) {
+    console.log("Error, getLogin:", err);
+  }
 };
 
 restaurantController.getSignup = (req: Request, res: Response) => {
-    try {
-        console.log("getSignup")
-        res.send("Signup Page");
-    } catch (err) {
-        console.log('Error, getSignup:', err);
-        res.send(err);
-    }
+  try {
+    console.log("getSignup");
+    res.send("Signup Page");
+  } catch (err) {
+    console.log("Error, getSignup:", err);
+    res.send(err);
+  }
 };
 
-
 restaurantController.processLogin = async (req: Request, res: Response) => {
-    try {
-        console.log("processLogin");
-        console.log("body:", req.body)
-        const input: LoginInput = req.body;
+  try {
+    console.log("processLogin");
+    console.log("body:", req.body);
+    const input: LoginInput = req.body;
 
-        const memberService = new MemberService();
-        const result = await memberService.processLogin(input)
-        
-        res.send(result);
-    } catch (err) {
-        console.log('Error, processLogin:', err);
-        res.send(err);
-    }
+    const memberService = new MemberService();
+    const result = await memberService.processLogin(input);
+
+    res.send(result);
+  } catch (err) {
+    console.log("Error, processLogin:", err);
+    res.send(err);
+  }
 };
 
 restaurantController.processSignup = async (req: Request, res: Response) => {
-    try {
-        console.log("processSignup")
+  try {
+    console.log("processSignup");
 
-        const newMember: MemberInput = req.body;
-        newMember.memberType = MemberType.RESTAURANT;
+    const newMember: MemberInput = req.body;
+    newMember.memberType = MemberType.RESTAURANT;
 
-        const memberService = new MemberService();
-        const result = await memberService.processSignup(newMember)
-        res.send(result)
-    } catch (err) {
-        console.log('Error, processSignup:', err);
-        res.send(err);
-    }
+    const memberService = new MemberService();
+    const result = await memberService.processSignup(newMember);
+    res.send(result);
+  } catch (err) {
+    console.log("Error, processSignup:", err);
+    res.send(err);
+  }
 };
 
 export default restaurantController;
-
 
 //CLUSTER ,DB, COLLECTION ,DOCUMENT ,DATASET  -- bu narsalar Member.modeli
 //  ichida ishlatkan codlarimizni nomlanishi 1234
--- src/router-admin.ts
+++ src/router-admin.ts
@@ -1,18 +1,16 @@
-import express from "express"
+import express from "express";
 const routerAdmin = express.Router();
 import restaurantController from "./controllers/restaurant.controller";
 
 //  Restoraurant
 routerAdmin.get("/", restaurantController.goHome);
 routerAdmin
-    .get("/login", restaurantController.getLogin)
-    .post("/login", restaurantController.processLogin);
+  .get("/login", restaurantController.getLogin)
+  .post("/login", restaurantController.processLogin);
 routerAdmin
-    .get("/signup", restaurantController.getSignup)
-    .post("/signup", restaurantController.processSignup);
+  .get("/signup", restaurantController.getSignup)
+  .post("/signup", restaurantController.processSignup);
 // Product
 
 //  User
 export default routerAdmin;
-
-
