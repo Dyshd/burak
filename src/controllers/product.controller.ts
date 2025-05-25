c832deaf7dbf3292fbabae2f2b5a691b53ab6209
Muhammad <zoirovm823@gmail.com>
2025-05-25 17:49:37 +0500

    feat: createNewProduct business login by restaurant member

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,45 +1,70 @@
 import { Request, Response } from "express";
-import Errors from "../libs/Errors";
+import Errors, { HttpCode, Message } from "../libs/Errors";
 import { T } from "../libs/types/common";
 import ProductService from "../models/Product.service";
+import { ProductInput } from "../libs/types/product";
+import { AdminRequest } from "../libs/types/member";
 
 const productService = new ProductService();
 
 const productController: T = {};
+/**SPA */
+
+/**SSR */
+
 productController.getAllProducts = async (req: Request, res: Response) => {
-    try {
-      console.log("getAllProducts");
-      res.render("products")
-    } catch (err) {
-      console.log("Error,getAllProducts:", err);
-      if(err instanceof Errors) res.status(err.code).json(err);
-      else res.status(Errors.standard.code).json(Errors.standard);
-    }
-  };
+  try {
+    console.log("getAllProducts");
+    res.render("products");
+  } catch (err) {
+    console.log("Error,getAllProducts:", err);
+    if (err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standard.code).json(Errors.standard);
+  }
+};
 
+productController.createNewProduct = async (
+  req: AdminRequest,
+  res: Response
+) => {
+  try {
+    console.log("createNewProduct");
+    console.log("req", req.files);
 
-  productController.createNewProduct = async (req: Request, res: Response) => {
-    try {
-      console.log("createNewProduct");
-      res.send("DONE")
-      
-    } catch (err) {
-      console.log("Error,createNewProduct:", err);
-      if(err instanceof Errors) res.status(err.code).json(err);
-      else res.status(Errors.standard.code).json(Errors.standard);
-    }
-  };
+    if (!req.files?.length)
+      throw new Errors(HttpCode.INTERNAL_SERVER_ERROR, Message.CREATE_FAILED);
 
+    const data: ProductInput = req.body;
+    data.productImages = req.files.map((ele) => {
+      return ele.path.replace(/\\/g, "/");
+    });
 
-  productController.updateChosenProduct = async (req: Request, res: Response) => {
-    try {
-      console.log("updateChosenProduct");
-      
-    } catch (err) {
-      console.log("Error,updateChosenProduct:", err);
-      if(err instanceof Errors) res.status(err.code).json(err);
-      else res.status(Errors.standard.code).json(Errors.standard);
-    }
-  };
+    await productService.createNewProduct(data);
+
+    res.send(
+      `<script> alert("Sucessful creation"); window.location.relace('admin/product/all') </script>`
+    );
+
+    console.log("data", data);
+    res.send("DONE");
+  } catch (err) {
+    console.log("Error,createNewProduct:", err);
+    const message =
+      err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG;
+      res.send(
+        `<script> alert("${message}"); window.location.relace('admin/product/all') </script>`
+      );
+  }
+};
+
+productController.updateChosenProduct = async (req: Request, res: Response) => {
+  try {
+    console.log("updateChosenProduct");
+  } catch (err) {
+    console.log("Error,updateChosenProduct:", err);
+    if (err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standard.code).json(Errors.standard);
+  }
+};
 
 export default productController;
